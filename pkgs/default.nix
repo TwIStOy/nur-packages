4ebb002326568e1679d605dd118059cf9ec5a3b1
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-06 15:13:32 +0800

    feat: add pkg maple-font-nf

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,8 +11,9 @@ lib.makeScope newScope
     gersemi = callPackage ./gersemi {};
     rime-ls = callPackage ./rime-ls {};
     vimPlugins = pkgs.recurseIntoAttrs (callPackage ./vim-plugins {
-      buildVimPlugin = pkgs.vimUtils.buildVimPlugin;
+      inherit (pkgs.vimUtils) buildVimPlugin;
     });
     distant = callPackage ./distant {};
+    maple-mono-nf = callPackage ./maple-mono-nf {};
   }
 )
---
+++ pkgs/maple-mono-nf/default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  unzip,
+}:
+pkgs.stdenv.mkDerivation rec {
+  version = "v7.0-beta8";
+  src = builtins.fetchurl {
+    url = "https://github.com/subframe7536/maple-font/releases/download/${version}/MapleMono-nf.zip";
+    sha256 = "08a1q6dfxmb259wd80aqnkabb24yaf25a12dcf6pik9n9rmxg5b5";
+  };
+  nativeBuildInputs = [unzip];
+  sourceRoot = ".";
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm64 *.ttf -t $out/share/fonts/truetype
+
+    runHook postInstall
+  '';
+}
